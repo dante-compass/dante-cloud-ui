398611ea942815806e955f63161796c2af1d8111
码匠君 <pointer_v@qq.com>
2026-09-07 23:10:13 +0800

    perf(components): [优化] 前端 Duration 自定义组件增加 “周”、“月”、“年” 选项

--- shared/components/src/lib/utils/constants.ts
+++ shared/components/src/lib/utils/constants.ts
@@ -1,8 +1,11 @@
-import type { SelectItem } from '../declarations';
+import type { SelectItem } from "../declarations";
 
 export const DURATION_UNITS: Array<SelectItem<string>> = [
-  { text: '天', value: 'days' },
-  { text: '小时', value: 'hours' },
-  { text: '分', value: 'minutes' },
-  { text: '秒', value: 'seconds' },
+  { text: "年", value: "years" },
+  { text: "月", value: "months" },
+  { text: "周", value: "weeks" },
+  { text: "天", value: "days" },
+  { text: "小时", value: "hours" },
+  { text: "分", value: "minutes" },
+  { text: "秒", value: "seconds" },
 ];
